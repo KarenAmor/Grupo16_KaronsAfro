6659220c8ad70481d47e3d9ee5d1de8bc1ad160a
SharonFlorez <shdflorezs35@gmail.com>
2021-12-18 10:51:30 -0500

    add: lógica del register, con subida de imagen y guardado en JSON

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT ||3000;
 const methodOverride = require('method-override')
 
 const rutas = require("./routers/main.js")
+const users = require("./routers/user.js")
 
 app.set('view engine', 'ejs');
 
@@ -20,62 +21,10 @@ app.use(express.static(publicFolderPath));
 app.use(methodOverride('_method'));
 
 app.use('/', rutas);
-app.use('/login', rutas);
-app.use('/producto/cabello', rutas);
-app.use('/producto/piel', rutas);
-app.use('/producto/maquillaje', rutas);
-app.use('/producto/accesorios', rutas);
-app.use('/producto/detail', rutas);
-app.use('/register', rutas);
-app.use('/carrito_compras', rutas);
-app.use('/quienesSomos', rutas);
-app.use('/administrador', rutas);
-app.use('/administrador/addProduct', rutas);
-app.use('/editProduct', rutas);
-app.use('/administrador/editProduct', rutas);
-app.use('/administrador/update/:id', rutas);
-app.use('/administrador/delete/:id', rutas);
+app.use('/', users);
+
 
 
 app.listen(port, ()=> console.log(`Servidor corriendo ${port}`))
-
-
-
-// app.get('/', (req, res)=> {
-//     res.sendFile(path.resolve(__dirname, './views/home.html'));
-// });
-
-// app.get("/login",(req,res)=>{
-//     res.sendFile(path.resolve(__dirname, "./views/login.html"))
-// });
-// app.get('/cabello', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './views/cabello.html'));
-// });
-// app.get('/piel', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './views/piel.html'));
-// });
-// app.get('/maquillaje', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './views/maquillaje.html'));
-// });
-// app.get('/accesorios', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './views/accesorios.html'));
-// });
-
-// app.get(('/register'), (req, res) =>{
-//     res.sendFile(path.join(__dirname, './views/register.html'));
-// })
-
-// app.get(('/carrito_compras'), (req, res) =>{
-//     res.sendFile(path.join(__dirname, './views/productCart.html'));
-// })
-
-// app.get('/quienesSomos', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './views/quienesSomos.html'));
-// });
-
-// app.get('/administrador', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './views/administrador.html'));
-// });
-
 
 

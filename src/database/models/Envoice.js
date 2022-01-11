64b0d6ba672fd76c42207dd1fccd19dab7a34291
KarenAmor <karedimor@gmail.com>
2022-01-10 22:19:29 -0500

    Se realizo diagrama de base de datos, las tablas y los modelos de estas mismas; users, products y envoice

---
+++ src/database/models/Envoice.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = 'Envoice'; 
+  let cols = {
+      id:  {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+
+  shopping_id: {
+    field: 'shopping_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: SHOPPING_TABLE,
+      key: 'id'
+    },
+   },
+ 
+  user_id: {
+    field: 'user_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: USER_TABLE,
+      key: 'id'
+    },
+   
+  }
+};
+let config = {
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+  deletedAt: false
+}
+const Envoice= sequelize.define(alias, cols, config);
+
+Envoice.associate = function(models) {
+  Envoice.hasMany(models.User, { 
+            as: "envoice", 
+            foreignKey: "user_id"
+        })
+Envoice.belongsToMany(models.Shopping, { 
+          as: "shopping",
+          foreignKey: 'envoice_id',
+          otherKey: 'product_id',
+          timestamps: false
+      })
+    }
+
+    return Envoice
+};
+
+
---
+++ src/database/models/User.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = 'User'; 
+  let cols  = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  first_name: {
+    type: dataTypes.STRING(100),
+    allowNull: false
+},
+last_name: {
+  type: dataTypes.STRING(100),
+  allowNull: false
+},
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  //confirmar contraseña
+  role: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  //avatar
+  avatar: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  
+};
+let config = {
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+  deletedAt: false
+};
+const User = sequelize.define(alias,cols,config);
+
+User.associate = function (models) {
+  User.hasMany(models.Envoice, { 
+        as: "users",
+        foreignKey: "users_id"
+    })
+  }
+
+return User
+};

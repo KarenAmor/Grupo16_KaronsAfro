80f8514982676aadf43cb8d072272c3ebae619ed
caredlav <caredlav@gmail.com>
2022-01-27 17:16:26 -0500

    Correcion en vista quienesSomos y ocultando credenciales en config.js.

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,26 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
+const env = process.env;
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "1234",
-    "database": "karonsafros_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "3307"
+    "username": env.USER,
+    "password": env.DATABASE_PASS,
+    "database": env.DATABASE_NAME,
+    "host": env.DATABASE_HOST,
+    "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": env.USER,
+    "password": env.DATABASE_PASS,
+    "database": env.DATABASE_NAME,
+    "host": env.DATABASE_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.USER,
-    "password": process.env.DATABASE_PASS,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
+    "username": env.USER,
+    "password": env.DATABASE_PASS,
+    "database": env.DATABASE_NAME,
+    "host": env.DATABASE_HOST,
     "dialect": "mysql"
   }
 }

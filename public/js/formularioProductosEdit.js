c071d1a295c96f65064fed47fa1a8ba5ad750826
KarenAmor <karedimor@gmail.com>
2022-02-14 14:38:36 -0500

    Validacion del formulario de agregar y editar producto

---
+++ public/js/formularioProductosEdit.js
@@ -0,0 +1,93 @@
+window.addEventListener("load", function(){
+    let formulario = document.querySelector("form.register")
+
+    formulario.addEventListener("submit", function(e){
+        const errores =[];
+
+        let campoNombreProducto = document.querySelector(".nombreProducto input");
+        
+        if (campoNombreProducto.value=="") {
+            errores.push("El campo Nombre del Producto debe estar completo")
+            campoNombreProducto.style.border="2px solid #be1c29"; 
+        } else if (campoNombreProducto.value.length<5){
+            errores.push("El campo nombre del producto debe tener al menos 5 caracteres")  
+            campoNombreProducto.style.border="2px solid #be1c29"; 
+        }else{
+            campoNombreProducto.style.border="2px solid #198754"
+        }
+
+        let campoPrecio = document.querySelector(".precioProducto input");
+        
+        if (campoPrecio.value=="") {
+            errores.push("El campo no debe estar vacio")
+            campoPrecio.style.border="2px solid #be1c29"; 
+        } else if (campoPrecio.value.length<5){
+            errores.push("El campo precio debe tener al menos 5 caracteres")  
+            campoPrecio.style.border="2px solid #be1c29"; 
+        }else{
+            campoPrecio.style.border="2px solid #198754"
+        }
+
+        let campoReferencia = document.querySelector(".referenciaProducto input");
+        
+        if (campoReferencia.value=="") {
+            errores.push("El campo referencia no debe estar vacio")
+            campoReferencia.style.border="2px solid #be1c29"; 
+        } else if (campoReferencia.value.length<0){
+            errores.push("El campo referencia debe tener al menos 1 caracter") 
+            campoReferencia.style.border="2px solid #be1c29"; 
+        }else{
+            campoReferencia.style.border="2px solid #198754"
+        }
+        let campoCantidadDisponible = document.querySelector(".cantidadProducto input");
+        
+        if (campoCantidadDisponible.value=="") {
+            errores.push("El campo cantidad Producto no debe estar vacio")
+            campoCantidadDisponible.style.border="2px solid #be1c29"; 
+        } else if (campoCantidadDisponible.value.length<0){
+            errores.push("El campo cantidad Producto debe tener al menos 1 caracter") 
+            campoCantidadDisponible.style.border="2px solid #be1c29";  
+        }else{
+            campoCantidadDisponible.style.border="2px solid #198754"
+        }
+        let campoDescripcionProducto = document.querySelector(".product.description input");
+        
+        if (campoDescripcionProducto.value=="") {
+            errores.push("El campo Descripcion del Producto  no debe estar vacio")
+            campoDescripcionProducto.style.border="2px solid #be1c29"; 
+        } else if (campoCantidadDisponible.value.length<20){
+            errores.push("El campo Descripcion del Producto  debe tener al menos 20 caracteres")
+            campoDescripcionProducto.style.border="2px solid #be1c29";   
+        }else{
+            campoDescripcionProducto.style.border="2px solid #198754"
+        }
+
+        let campoImagenProducto = document.querySelector(".imagenProducto input");
+        const formatoImagenProductoValido =/.(gif|jpeg|jpg|png)$/i;
+        
+        if(campoImagenProducto.value == ""){
+            errores.push('Debes subir una imagen del producto');
+            campoImagenProducto.style.border="2px solid #be1c29"; 
+        }else if(!formatoImagenProductoValido.exec(campoImagenProducto.value)){
+            errores.push('El campo Imagen del producto debe tener una imagen en formato JPG, JPEG, PNG y/o GIF');
+            campoImagenProducto.style.border="2px solid #be1c29";            
+        }else {
+            campoImagenProducto.style.border="2px solid #198754";
+        };
+
+    });
+    if (errores.length > 0) {
+        e.preventDefault();
+        let ulErrors = document.querySelector('.errores');
+        ulErrors.classList.add('alert-warning');
+        alert('Ups! Parece que hay inconvenientes con alguno de los campos.')
+        for (let i = 0; i < errores.length; i++) {
+            ulErrors.innerHTML += "<li>" +  errores[i] + "</li>"; 
+        };
+
+    }else{
+        alert('El registro se ha realizado 🙂');
+            formulario.submit();
+        }
+})
+
